98c55a80aac27e6df2326b420bf477d4510cfbbc
Mauricio Rodas <mauriciorodascontacto@gmail.com>
2025-10-10 22:01:54 -0300

    Propiedades de Dominio

--- Gestor de Catalogo/Dominio/Articulo.cs
+++ Gestor de Catalogo/Dominio/Articulo.cs
@@ -8,5 +8,13 @@ namespace Dominio
 {
     public class Articulo
     {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public string Imagen { get; set; }
+        public Decimal Precio { get; set; }
     }
 }
