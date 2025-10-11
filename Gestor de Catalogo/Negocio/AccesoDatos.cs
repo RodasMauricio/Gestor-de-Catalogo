42d36a66786d7e0bba3d2591359bbdf6d1cab1e5
Mauricio Rodas <mauriciorodascontacto@gmail.com>
2025-10-10 21:41:41 -0300

    Lógica de AccesoDatos - Conexión a DB (App.config)

--- Gestor de Catalogo/Negocio/AccesoDatos.cs
+++ Gestor de Catalogo/Negocio/AccesoDatos.cs
@@ -3,10 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Negocio
 {
     public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["CATALOGO_DB"].ConnectionString;
+            conexion = new SqlConnection(connectionString);
+            comando = new SqlCommand();
+        }
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public void Consulta(string c)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = c;
+        }
+
+        public void Parametros(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void EjecutarConsulta()
+        {
+            comando.Connection = conexion;
+            conexion.Open();
+            lector = comando.ExecuteReader();
+        }
+
+        public void EjecutarComando()
+        {
+            comando.Connection = conexion;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
     }
 }
